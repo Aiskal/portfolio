efe26f23c571574fc9cf02e43f6703656c368255
aiskal <icingdawn@gmail.com>
2024-11-20 00:04:10 +0100

    ADD/Jsdata

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+import dataSkins from "./skins.js";
+
 document.addEventListener('mousemove', function (e) {
     const chance = Math.random();
 
@@ -39,3 +41,34 @@ function createPixel(x, y) {
         pixel.remove();
     }, 1000);
 }
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const heroUl = document.querySelector('#hero-ul');
+    const heroImage = document.querySelector('#right picture img');
+    const textHero = document.querySelector('#text-hero i');
+
+    Object.entries(dataSkins.skins).forEach(([name, details], index) => {
+        const li = document.createElement('li');
+        if (index === 0) li.classList.add('active');
+
+        const img = document.createElement('img');
+        img.src = `./src/img/${details.img}.png`;
+        img.alt = name;
+
+        li.appendChild(img);
+        heroUl.appendChild(li);
+
+        li.addEventListener('click', () => {
+            heroImage.src = `./src/img/${details.img}.png`;
+            textHero.textContent = name;
+
+            document.querySelectorAll('#hero-ul li').forEach((item) => item.classList.remove('active'));
+            li.classList.add('active');
+        });
+
+        if (index === 0) {
+            heroImage.src = `./src/img/${details.img}.png`;
+            textHero.textContent = name;
+        }
+    });
+});
---
+++ src/skins.js
@@ -0,0 +1,15 @@
+const dataSkins = {
+    "skins": {
+        "Professeur": {
+            "img": "professeur"
+        },
+        "Futurist": {
+            "img": "vr"
+        },
+        "Past self": {
+            "img": "minime"
+        }
+    }
+};
+
+export default dataSkins;
